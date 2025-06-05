69a13f5a7fcf06ff638a24b88ff7e97a35e335ac
matsob0123 sob <matsob0123@gmail.com>
2025-06-05 15:21:33 +0000

    ee

--- limbo/run.sh
+++ limbo/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+WORKDIR="${WORKDIR:-/config/limbo}"
+COMMAND="${COMMAND:-java -jar LIMBO.jar --nogui}"
+
 echo "Przechodzę do katalogu: $WORKDIR"
 cd "$WORKDIR" || { echo "Nie mogę wejść do $WORKDIR"; exit 1; }
 
