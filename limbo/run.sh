1e37626f296e2c30ac9d6a6ebf723490b15e5e1f
matsob0123 sob <matsob0123@gmail.com>
2025-06-05 15:29:15 +0000

    eee

--- limbo/run.sh
+++ limbo/run.sh
@@ -5,8 +5,6 @@ CONFIG_FILE="/data/options.json"
 WORKDIR=$(jq -r '.WORKDIR // "/config/limbo"' "$CONFIG_FILE")
 COMMAND=$(jq -r '.COMMAND // "java -jar limbo.jar --nogui"' "$CONFIG_FILE")
 
-exec ls -la /config
-
 echo "Przechodzę do katalogu: $WORKDIR"
 cd "$WORKDIR" || { echo "❌ Nie mogę wejść do katalogu $WORKDIR"; exit 1; }
 
